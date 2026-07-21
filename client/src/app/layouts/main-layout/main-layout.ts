86ab4520b4ce46fff632687ac385359f39158d59
Mohammad <shakouri1380@gmail.com>
2026-07-21 11:00:57 +0330

    fix main layout and nav bar styles

--- client/src/app/layouts/main-layout/main-layout.ts
+++ client/src/app/layouts/main-layout/main-layout.ts
@@ -6,32 +6,13 @@ import { DatePicker } from '../../components/date-picker/date-picker';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterOutlet, NavBar, DatePicker],
+  imports: [RouterOutlet, NavBar],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss',
 })
 export class MainLayout {
-    direction: string;
 
   constructor(private appEnv: AppEnvService) {
-    this.direction = this.appEnv.direction;
-  }
-  signaleCount = signal<number>(0);
-  simpleCount = 0;
-
-  isAuthenticated = false;
-  userStatus = 'active';
-
-  handleLogin() {
-    this.isAuthenticated = true;
-    this.signaleCount.update((count) => count + 2);
-    this.simpleCount += 2;
-    console.log('AppEnvService api base url:', this.appEnv.apiBaseUrl);
-  }
-
-  handleLogout() {
-    this.isAuthenticated = false;
-    this.signaleCount.update((count) => count - 1);
-    this.simpleCount -= 1;
+      console.log('AppEnvService api base url:', this.appEnv.apiBaseUrl);
   }
 }
