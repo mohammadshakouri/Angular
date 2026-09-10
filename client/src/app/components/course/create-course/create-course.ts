055d40760ec554627ced21d645e7a466031df61d
Mohammad <shakouri1380@gmail.com>
2026-09-10 22:12:23 +0330

    signal form

--- client/src/app/components/course/create-course/create-course.ts
+++ client/src/app/components/course/create-course/create-course.ts
@@ -1,8 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { createCourseDto } from '../../../dtoS/courseDto';
 import { FormsModule } from '@angular/forms';
 import { CourseService } from '../../../services/courseService';
 import { injectI18n } from '../../../services/caption.service';
+import { form,FormField } from '@angular/forms/signals';
 
 @Component({
   selector: 'create-course',
@@ -14,23 +15,13 @@ export class CreateCourse {
   private courseService: CourseService = inject(CourseService);
   i18n = injectI18n();
 
-  course: createCourseDto = {
+  course = signal<createCourseDto>({
     title: '',
     description: '',
     price: 0,
-  };
+  });
 
-  handleCreateCourse() {
-    if (this.course.title && this.course.description && this.course.price > 0) {
-      this.courseService.addCourse(this.course);
-      // Reset the form
-      this.course = {
-        title: '',
-        description: '',
-        price: 0,
-      };
-    } else {
-      alert('Please fill in all fields and ensure price is greater than 0.');
-    }
-  }
+  courseForm = form(this.course);
+
+  handleCreateCourse() {}
 }
